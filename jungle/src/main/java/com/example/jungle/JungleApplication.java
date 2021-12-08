1b9470c9020da7ab9a6c4b7f63b25ee924154cc0
김정글 <38073220+youngmeen@users.noreply.github.com>
2021-12-09 01:31:55 +0900

    first commit
    first commit

---
+++ jungle/src/main/java/com/example/jungle/JungleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jungle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungleApplication.class, args);
+	}
+
+}
---
+++ jungle/src/test/java/com/example/jungle/JungleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jungle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
